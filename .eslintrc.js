5e093daa2a33c1cb1ff893721aa46827988e3340
Sehyun <sehyun.kim@themobilityone.com>
2024-03-17 21:36:36 +0900

    style: change lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,12 @@
 module.exports = {
   env: {
-    browser: true,
+    commonjs: true,
     node: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'react-app', 'plugin:react-hooks/recommended'],
+  extends: 'eslint:recommended',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
   },
-  plugins: ['react', 'react-hooks'],
-  rules: {
-    'no-unused-vars': 'off',
-    'react-hooks/exhaustive-deps': 'off',
-  },
+  rules: {},
 };
